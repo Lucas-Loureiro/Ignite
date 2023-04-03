5c6f376d7d15a94ca57f3977cc0583b20828e4f3
Lucas-Loureiro <loureiro4778@gmail.com>
2023-04-03 12:32:20 -0300

    First commit

---
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,53 @@
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
+import styles from './Post.module.css';
+
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <Avatar src="https://github.com/Lucas-Loureiro.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Lucas Loureiro</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='02 de Fevereiro às 08:13h' dateTime='2022-02-02 08:13:30'>Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+                <p>👉 {' '}<a href="jane.design/doctorcare">jane.design/doctorcare</a></p>
+
+                <p>
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href="">#nlw </a>{' '}
+                    <a href="">#rocketseat</a>
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea
+                    placeholder='Deixe um comentário'
+                />
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
+
+            <div className='commentList'>
+                <Comment />
+                <Comment />
+                <Comment />
+            </div>
+        </article>
+    );
+}
