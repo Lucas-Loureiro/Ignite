e258ecd9b1ac5f936b4a182b081376b323d13d4e
Lucas-Loureiro <loureiro4778@gmail.com>
2023-06-18 01:38:44 -0300

    Checkbox customizado

--- Desafio_01/src/components/Task.tsx
+++ Desafio_01/src/components/Task.tsx
@@ -5,9 +5,11 @@ export function Task() {
     return (
         <div className={styles.task}>
 
-            <input type="checkbox" id="task1" />
-            <label htmlFor="task1">Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</label>
-            <Trash size={16}/>
+            <input type="checkbox"  />
+            <label >Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</label>
+            <button  title='Deletar Tarefa'>
+                <Trash size={16} />
+            </button>
         </div>
 
     );
