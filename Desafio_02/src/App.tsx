ae6ea73e748f5d0fc687d0da84486f5960351908
Lucas Loureiro <lucas.vasconcellos@impar.com.br>
2024-01-15 23:45:01 -0300

    Criando rotas

--- Desafio_02/src/App.tsx
+++ Desafio_02/src/App.tsx
@@ -1,8 +1,17 @@
-export function App() {
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./components/Router";
 
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
   return (
-    <>
-      
-    </>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
